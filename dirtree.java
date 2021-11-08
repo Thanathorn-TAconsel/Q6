0b8c663ea0ae1e47985a16cf880e31fd9f139da9
Thanathorn-TAconsel <s6301012610043@email.kmutnb.ac.th>
2021-11-08 11:58:51 +0700

    GUI DISPLAY

--- dirtree.java
+++ dirtree.java
@@ -1,8 +1,26 @@
 import java.io.File;
+import java.util.ArrayList;
+
+import javax.swing.JButton;
+import javax.swing.JFrame;
 
 class dirtree{
+    JFrame f = new JFrame();
+    ArrayList<JButton> bs = new ArrayList<JButton>();
     dirtree() {
+        f.setBounds(100,100,400,600);
+        f.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        f.setLayout(null);
+        f.setVisible(true);
         realSearch("/home/ta/Desktop/New directory/Processing",1);
+    }
+    private void addButton(String name,int level) {
+        JButton b = new JButton(name);
+        b.setLocation(level * 20,bs.size() * 30);
+        b.setSize(200,30);
+        bs.add(b);
+        f.repaint();
+        f.add(b);
     }
     public static void main(String args[]) {
         new dirtree();
@@ -19,6 +37,7 @@ class dirtree{
                 System.out.print("\t");
             }
             System.out.println(names[i]);
+            addButton(names[i], level);
             printlist(dir + "/" + names[i], level+1,limitlevel);
           }
         }
